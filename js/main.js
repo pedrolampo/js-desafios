748f00487394cc02134a56f704c6b8823aa4ab58
Pedro Sanchez <pedrolampo50@gmail.com>
2021-09-01 11:26:38 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let nombre = prompt('Ingrese su nombre');
+
+console.log('¡Buenos días,' + ' ' + nombre + '!');
+
+let numGenerico = 4;
+let numFav = prompt('Ingresa tu número favorito');
+let numFav2 = parseInt(numFav);
+
+console.log(numGenerico + numFav2);
+
+let randomText = prompt('Escriba una oración cualquiera');
+let randomText2 = prompt('Escriba otra oración');
+let fusionTxt = randomText + ' ' + randomText2;
+
+alert(fusionTxt)
