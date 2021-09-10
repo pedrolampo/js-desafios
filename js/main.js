74167b34fe8bf4272c6ef223a81bb507dc5d565d
Pedro Sanchez <pedrolampo50@gmail.com>
2021-09-10 15:26:39 -0300

    Desafío clase 3

--- js/main.js
+++ js/main.js
@@ -1,38 +1,13 @@
-let numeroIngresado = prompt('Ingrese un número:');
-let numero = parseInt(numeroIngresado);
+let numeroIngresado = parseInt(prompt('Ingrese un número par:'));
 
-if (numero < 10) {
-  alert('El número ingresado tiene 1 cifra.');
-  console.log('El número ingresado tiene 1 cifra.');
-} else if (numero < 100) {
-  alert('El número ingresado tiene 2 cifras.');
-  console.log('El número ingresado tiene 2 cifras.');
-} else if (numero < 1000) {
-  alert('El número ingresado tiene 3 cifras.');
-  console.log('El número ingresado tiene 3 cifras.');
-} else {
-  alert('El número ingresado tiene 4 cifras o más.');
-  console.log('El número ingresado tiene 4 cifras o más.');
-}
+let numPar = numeroIngresado % 2;
 
-let txtIngresado = prompt('Escriba "Buenos días":');
+switch (numPar) {
+  case 0:
+    console.log('Perfecto!');
+    break;
 
-if (txtIngresado === 'Buenos días') {
-  console.log('El usuario respetó la consigna.');
-} else {
-  console.log('El usuario no respetó la consigna.');
-}
-
-let numeroIngresado2 = prompt('Ingrese otro número:');
-let numero2 = parseInt(numeroIngresado2);
-
-if (numero2 < 50) {
-  alert('El número ingresado es menor a 50.');
-  console.log('El número ingresado es menor a 50.');
-} else if (numero2 > 100) {
-  alert('El número ingresado es mayor a 100.');
-  console.log('El número ingresado es mayor a 100.');
-} else {
-  alert('El número ingresado está entre 50 y 100.');
-  console.log('El número ingresado está entre 50 y 100.');
+  default:
+    console.log('El número ingresado no es par.');
+    break;
 }
